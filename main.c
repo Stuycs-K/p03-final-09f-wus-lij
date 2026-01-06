5d9d5cd9895e89435a9c80d81bd2dd35687a974f
Brian <brian38444@gmail.com>
2026-01-06 14:29:49 -0500

    empty files created

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+#include <dirent.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main(){
+
+}
