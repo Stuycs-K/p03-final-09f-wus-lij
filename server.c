1ab5a46e86887596dc6d5005b7f8668a10838340
Steven Wu <steveph945@gmail.com>
2026-01-08 14:15:43 -0500

    small fix

--- server.c
+++ server.c
@@ -9,13 +9,13 @@ int main(int argc, char *argv[] ) {
   char client_name[BUFFER_SIZE];
   requestName(server_name);
   int listen_socket = server_setup();
-  
+
 
   int client_socket = server_tcp_handshake(listen_socket);
   int bytes = recv(client_socket, client_name, BUFFER_SIZE, 0);
   if (bytes > 0){
     client_name[bytes] = '\0';
   }
-  printf("Your opponent:%s", client_name);
+  printf("Your opponent: %s", client_name);
   send(client_socket, server_name, strlen(server_name), 0);
 }
